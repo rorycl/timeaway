7ce0a10fb43feab02f2ca21eab6ef2c3cca7276c
rorycl <gitrl@campbell-lange.net>
2023-09-17 14:39:17 +0100

    faas: add GCP cloud function entry point

---
+++ faas.go
@@ -0,0 +1,22 @@
+package faas
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rorycl/timeaway/web"
+)
+
+// GCPServer is the entry point for a FAAS application as set out in
+// section 4.5 of Joel Holmes' "Shipping Go". The idea for multiple
+// endpoints is discussed at
+// https://medium.com/google-cloud/hack-use-cloud-functions-as-a-webserver-with-golang-42edc7935247
+func GCPServer(w http.ResponseWriter, r *http.Request) {
+	m := mux.NewRouter()
+	m.HandleFunc("/", web.Home)
+	m.HandleFunc("/home", web.Home)
+	m.HandleFunc("/favicon.ico", web.Favicon)
+	m.HandleFunc("/trips", web.Trips)
+	m.HandleFunc("/health", web.Health)
+	m.ServeHTTP(w, r)
+}
